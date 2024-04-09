8099890d810ca8b09d3de7905d5a50ec6e17d08d
KaranAmbhore <166363671+KaranAmbhore@users.noreply.github.com>
2024-04-09 16:34:14 +0530

    Update header.php

--- header.php
+++ header.php
@@ -2,14 +2,14 @@
         <div class="header_main">
 		<div class="navBar">
 			<ul>
-				<li><a href="index.php">Home</a></li>
-				<li><a href="index.php#aboutus">About Us</a></li>
-                <li><a href="index.php#contactus">Contact Us</a></li>
+				<li><a href="index.html">Home</a></li>
+				<li><a href="index.html#aboutus">About Us</a></li>
+                <li><a href="index.html#contactus">Contact Us</a></li>
 				<li><a href="staff_login.php">Staff Login</a></li>
 			
 			</ul>
 		</div>
-	 <a href="index.php"><div class="logo-name">
+	 <a href="index.html"><div class="logo-name">
 			<div class="logo">
              <img class="logo_img" src="img/chase.jpg">
 			</div>
